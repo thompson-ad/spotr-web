5679b1db46edd307645d05a68d39ec3272678cfa
Aaron Thompson <thompson_ad@outlook.com>
2024-07-31 19:04:02 +0100

    feat: add some links

--- app/routes/_marketing+/programmes+/index.tsx
+++ app/routes/_marketing+/programmes+/index.tsx
@@ -1,3 +1,14 @@
+import { Link } from '@remix-run/react'
+
 export default function ProgrammesRoute() {
-	return <div>Programmes</div>
+	return (
+		<>
+			<div>Programmes</div>
+			<div>
+				<Link relative="path" to="fullBody/dayOne">
+					Day 1
+				</Link>
+			</div>
+		</>
+	)
 }
