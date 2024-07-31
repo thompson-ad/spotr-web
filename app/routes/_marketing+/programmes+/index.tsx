14cd0db66c547ecd4a698b05e9a438c57ea81f9e
Aaron Thompson <thompson_ad@outlook.com>
2024-07-31 19:30:16 +0100

    feat: add dynamic routes

--- app/routes/_marketing+/programmes+/index.tsx
+++ app/routes/_marketing+/programmes+/index.tsx
@@ -1,13 +1,25 @@
 import { Link } from '@remix-run/react'
 
 export default function ProgrammesRoute() {
+	const programmes = [
+		{ id: 'fullBody', name: 'Full Body' },
+		{ id: 'upperBody', name: 'Upper Body' },
+		{ id: 'lowerBody', name: 'Lower Body' },
+	]
+
 	return (
 		<>
 			<div>Programmes</div>
 			<div>
-				<Link relative="path" to="fullBody/dayOne">
-					Day 1
-				</Link>
+				{programmes.map((programme) => (
+					<Link
+						key={programme.id}
+						relative="path"
+						to={`${programme.id}/dayOne`}
+					>
+						{programme.name}
+					</Link>
+				))}
 			</div>
 		</>
 	)
